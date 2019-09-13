12eb8a78ff61773dd6d7998c58f9e89062c0414e
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2019-09-13 22:00:41 +0300

    add store dev tools and router store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { SharedModule } from "./shared/shared.module";
 import * as fromApp from "./store/app.reducer";
 import { EffectsModule } from "@ngrx/effects";
 import { AuthEffects } from "./auth/store/auth.effects";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { environment } from "src/environments/environment";
+import { StoreRouterConnectingModule } from "@ngrx/router-store";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -20,7 +23,9 @@ import { AuthEffects } from "./auth/store/auth.effects";
     AppRoutingModule,
     SharedModule,
     StoreModule.forRoot(fromApp.appReducer),
-    EffectsModule.forRoot([AuthEffects])
+    EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({ logOnly: environment.production }),
+    StoreRouterConnectingModule.forRoot()
   ],
   providers: [
     {
